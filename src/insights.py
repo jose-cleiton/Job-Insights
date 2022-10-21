5cad0c78064c7332cbe56ccef2c8c7868f5e724f
jose-cleiton <cleitons835@gmail.com>
2022-10-21 17:43:44 -0300

    feat: :bug: tirando complexidade

--- src/insights.py
+++ src/insights.py
@@ -1,5 +1,3 @@
-from logging import exception
-
 from src.jobs import read
 
 
@@ -149,42 +147,14 @@ def get_min_salary(path):
 
 
 def matches_salary_range(job: dict, salary: int):
-    """Checks if a given salary is in the salary range of a given job
-
-    Parameters
-    ----------
-    job : dict
-        The job with `min_salary` and `max_salary` keys
-    salary : int
-        The salary to check if matches with salary range of the job
-
-    Returns
-    -------
-    bool
-        True if the salary is in the salary range of the job, False otherwise
-
-    Raises
-    ------
-    ValueError
-        If `job["min_salary"]` or `job["max_salary"]` doesn't exists
-        If `job["min_salary"]` or `job["max_salary"]` aren't valid integers
-        If `job["min_salary"]` is greather than `job["max_salary"]`
-        If `salary` isn't a valid integer
-    """
 
     try:
         salary_max = int(job["max_salary"])
         salary_min = int(job["min_salary"])
         salary = int(salary)
         assert salary_min <= salary_max
-    except AssertionError:
+    except (AssertionError, ValueError, TypeError, KeyError):
         raise ValueError("min_salary is greather than max_salary")
-    except ValueError:
-        raise ValueError("Invalid salary range")
-    except TypeError:
-        raise ValueError("Salary or salary range is not an integer")
-    except KeyError:
-        raise ValueError("Salary is missing")
     else:
         return salary >= salary_min and salary <= salary_max
 
