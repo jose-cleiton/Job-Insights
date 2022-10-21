7999dd6d79542b7c3484e93b6e64344ffbfa8617
jose-cleiton <cleitons835@gmail.com>
2022-10-21 14:15:11 -0300

    feat: :sparkles: Requisito 01

--- src/jobs.py
+++ src/jobs.py
@@ -1,3 +1,4 @@
+import csv
 from functools import lru_cache
 
 
@@ -15,4 +16,20 @@ def read(path):
     list
         List of rows as dicts
     """
-    return []
+    dados = []
+    with open(path, "r") as f:
+        reader = csv.DictReader(f)
+        for linha in reader:
+            dados.append(linha)
+    return dados
+
+
+if __name__ == "__main__":
+    # Path to file
+    path = "src/jobs.csv"
+
+    # Read the file
+    jobs = read(path)
+
+    # Print the first 5 rows
+    print(jobs[:5])
