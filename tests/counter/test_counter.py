75dfc1b118bdbe7b7c8a546f405543ed97524b77
jose-cleiton <cleitons835@gmail.com>
2022-10-22 09:29:32 -0300

    feat: :white_check_mark: Tipando

--- tests/counter/test_counter.py
+++ tests/counter/test_counter.py
@@ -1,9 +1,9 @@
 from src.counter import count_ocurrences
 
 
-def test_counter():
-    path = "src/jobs.csv"
-    word = "Python".lower()
+def test_counter() -> None:
+    path: str = "src/jobs.csv"
+    word: str = "Python".lower()
     assert count_ocurrences(path, "React") == 141, "Should be 141"
     assert count_ocurrences(path, word) == 1639, "Should be 1639"
     assert count_ocurrences(path, "Java") == 676, "Should be 676"
