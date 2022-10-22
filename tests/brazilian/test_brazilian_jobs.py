3d2d179e0475f55086e9337b09778145cdb65961
jose-cleiton <cleitons835@gmail.com>
2022-10-22 19:00:25 -0300

    feat: Implemente um teste para a função read_brazilian_file

--- tests/brazilian/test_brazilian_jobs.py
+++ tests/brazilian/test_brazilian_jobs.py
@@ -1,5 +1,36 @@
-# from src.brazilian_jobs import read_brazilian_file
+from unittest.mock import patch
+
+from src.brazilian_jobs import read_brazilian_file
 
 
 def test_brazilian_jobs():
-    pass
+    """
+    Teste se a função retorna os dados esperados"""
+    expected_of_returned = [
+        {
+            "title": "Analista de Sistemas",
+            "salary": "R$ 3.000,00",
+            "type": "CLT",
+        },
+        {
+            "title": "Analista de Sistemas",
+            "salary": "R$ 3.000,00",
+            "type": "CLT",
+        },
+    ]
+    with patch("src.brazilian_jobs.jobs.read") as mock:
+        mock.return_value = [
+            {
+                "titulo": "Analista de Sistemas",
+                "salario": "R$ 3.000,00",
+                "tipo": "CLT",
+            },
+            {
+                "titulo": "Analista de Sistemas",
+                "salario": "R$ 3.000,00",
+                "tipo": "CLT",
+            },
+        ]
+        assert (
+            read_brazilian_file("path") == expected_of_returned
+        ), "Deveria retornar os dados esperados"
