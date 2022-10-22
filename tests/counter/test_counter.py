7ec3460b0308ade37ce2c1e3817247da7c5260b5
jose-cleiton <cleitons835@gmail.com>
2022-10-22 09:22:27 -0300

    fix: :bug: Teste counter

--- tests/counter/test_counter.py
+++ tests/counter/test_counter.py
@@ -4,4 +4,6 @@ from src.counter import count_ocurrences
 def test_counter():
     path = "src/jobs.csv"
     word = "Python".lower()
-    assert count_ocurrences(path, word) == 3
+    assert count_ocurrences(path, "React") == 141, "Should be 141"
+    assert count_ocurrences(path, word) == 1639, "Should be 1639"
+    assert count_ocurrences(path, "Java") == 676, "Should be 676"
