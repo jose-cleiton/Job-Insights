f6d086b0bb1f4632f982d8731ab5ecbe3fd04058
jose-cleiton <cleitons835@gmail.com>
2022-10-23 17:16:38 -0300

    fix: :zap: Refazendo sem gitCopilot

--- tests/brazilian/test_brazilian_jobs.py
+++ tests/brazilian/test_brazilian_jobs.py
@@ -1,36 +1,10 @@
-from unittest.mock import patch
-
 from src.brazilian_jobs import read_brazilian_file
 
 
 def test_brazilian_jobs():
-    """
-    Teste se a função retorna os dados esperados"""
-    expected_of_returned = [
-        {
-            "title": "Analista de Sistemas",
-            "salary": "R$ 3.000,00",
-            "type": "CLT",
-        },
-        {
-            "title": "Analista de Sistemas",
-            "salary": "R$ 3.000,00",
-            "type": "CLT",
-        },
+    PATH = "tests/mocks/brazilians_jobs.csv"
+    expected = [
+        {"title": "Maquinista", "salary": "2000", "type": "trainee"},
+        {"title": "Motorista", "salary": "3000", "type": "full time"},
     ]
-    with patch("src.brazilian_jobs.jobs.read") as mock:
-        mock.return_value = [
-            {
-                "titulo": "Analista de Sistemas",
-                "salario": "R$ 3.000,00",
-                "tipo": "CLT",
-            },
-            {
-                "titulo": "Analista de Sistemas",
-                "salario": "R$ 3.000,00",
-                "tipo": "CLT",
-            },
-        ]
-        assert (
-            read_brazilian_file("path") == expected_of_returned
-        ), "Deveria retornar os dados esperados"
+    assert read_brazilian_file(PATH)[0:2] == expected
